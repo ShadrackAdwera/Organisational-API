ca1d1c902986a7ad00ebea4d41ca294a17c0b9c4
ADWESH <adweshshaddie@gmail.com>
2020-01-17 17:32:23 +0300

    add variables, getter and setter methods to the classes

--- src/main/java/models/News.java
+++ src/main/java/models/News.java
@@ -1,7 +1,38 @@
 package models;
 
+import java.util.Objects;
+
 public abstract class News {
     private String title;
     private String content;
-    
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof News)) return false;
+        News news = (News) o;
+        return Objects.equals(getTitle(), news.getTitle()) &&
+                Objects.equals(getContent(), news.getContent());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getTitle(), getContent());
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
